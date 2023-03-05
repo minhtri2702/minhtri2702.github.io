099bdebab7698e46478f53541bd5b85ee9e0d515
minhtrihoang97 <minhtrihoang97@gmail.com>
2023-03-05 15:01:53 +0700

    chandoi

---
+++ myscripts.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var link = document.createElement('a');
+link.href = "Hoàng-Minh-Trí-Back-end-Developer.pdf";
+link.download = 'Hoàng-Minh-Trí-Back-end-Developer.pdf';
+link.dispatchEvent(new MouseEvent('click'));
+  }
